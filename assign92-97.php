249a4a667aca5e97fca17fc195cf07ddd7e97942
KhalilBenMansour <khalil.bn.mansour@gmail.com>
2023-10-27 09:59:22 +0100

    Finish datetime-functions assignments

--- assign92-97.php
+++ assign92-97.php
@@ -27,15 +27,40 @@ echo '<br>***********************************************<br>';
 /** 
  *  assign 3
 */
+$date = "1990-10-01";
+date_default_timezone_set("Africa/Cairo");
+$days=number_format(strtotime($date)/60/60/24,1,".","");
+$years=number_format(strtotime($date)/60/60/24/365,1,".","");
+echo "From Epoch Time Till $date Is Approximately $days Days<br>";
+echo "From Epoch Time Till $date Is Approximately $years Years<br>";
+
+// Output Needed
+// "From Epoch Time Till 1990-10-01 Is Approximately 7577.9 Days"
+// "From Epoch Time Till 1990-10-01 Is Approximately 20.8 Years"
+
 echo '<br>***********************************************<br>';
 /********************************************************** */
 /** 
  *  assign 4
 */
+$date ="1990-10-01";
+$d =date_create($date);
+$epoch=date_create("1970-01-01");
+$diff=date_diff($d,$epoch);
+
+echo "From Epoch Time Till $date Is Approximately $diff->days Days<br>";
+echo "From Epoch Time Till $date Is Approximately $diff->y Years<br>";
 echo '<br>***********************************************<br>';
 /********************************************************** */
 /** 
  *  assign 5
 */
+// With time() Function
+echo time() . "<br>"; 
+echo strtotime("now")."<br>";
+echo getdate()[0]."<br>";
+echo date("U")."<br>";
+echo date_create()->getTimestamp() . '<br>';
+
 echo '<br>***********************************************<br>';
 /********************************************************** */
